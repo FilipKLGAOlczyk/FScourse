33715e5e9251f68e89e901fb9b5519d7aac24756
Filip Olczyk <261501@edu.p.lodz.pl>
2026-08-02 13:07:05 +0200

    part 2b phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonsForm from './components/PersonsForm'
+import Persons from './components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { 
+      name: 'Arto Hellas',
+      number: '040-123456'
+    },
+    {
+      name: 'Ada Lovelace',
+      number: '39-44-5323523'
+    },
+    {
+      name: 'Dan Abramov',
+      number: '12-43-234345'
+    }])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const handleFilterChange = (event) => {
+      console.log(event.target.value)
+      setFilter(event.target.value)
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    if (newNumber === '' || newName === '') {
+      alert('Name and/or number cannot be empty')
+      return
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }))
+    setNewName('')
+    setNewNumber('')
+  }
+
+
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+  
+  
+
+  
+
+
+  
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+        <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <h2>add a new</h2>
+      <PersonsForm 
+      newName={newName} 
+      newNumber={newNumber} 
+      handleNameChange={handleNameChange} 
+      handleNumberChange={handleNumberChange} 
+      handleSubmit={handleSubmit} />
+    <div>
+      <h2>Numbers</h2>
+    </div>
+    <div>
+      <Persons personsToShow={personsToShow} />
+    </div>
+  </div>
+  )
+}
+  export default App
