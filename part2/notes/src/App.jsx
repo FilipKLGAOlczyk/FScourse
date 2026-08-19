1b15f2cd33836ad3f8d1ec8784dace591dd2f732
Filip Olczyk <261501@edu.p.lodz.pl>
2026-08-19 21:03:03 +0200

    part2 css basics

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,11 +1,16 @@
 import Note from './components/Note'
 import { useState, useEffect } from 'react'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
 const App = () => {
 const [notes, setNotes] = useState([])
 const [newNote, setNewNote] = useState('')
 const [showAll, setShowAll] = useState(true)
+const [errorMessage, setErrorMessage] = useState('some error happened...')
+
+
 
 useEffect(() => {
   console.log('effect')
@@ -32,9 +37,12 @@ console.log('render', notes.length, 'notes')
         note.id === id ? returnedNote : note))
     })
     .catch(error => {
-      alert(
-        `the note '${note.content}' was already removed from server`
+      setErrorMessage(
+`Note '${note.content}' was already removed from server`
       )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
       setNotes(notes.filter(n => n.id !== id))
     })
   }
@@ -63,6 +71,7 @@ const addNote = (event) => {
 return (
   <div>
     <h1>Notes</h1>
+    <Notification message={errorMessage} />
     <div>
       <button onClick={() => setShowAll(!showAll)}>
         show {showAll ? 'important' : 'all'}
@@ -84,6 +93,7 @@ return (
       />
       <button type="submit">save</button>
     </form>
+    <Footer />
   </div>
 )
 }
